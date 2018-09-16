c7a552a0b832d228357a6b0b3171191a92819db3
Peder Voldnes Langdal <peder.langdal@gmail.com>
2018-09-16 21:24:03 +0200

    First commit

---
+++ Pic2Vector/Processing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace Pic2Vector
+{
+    public static class Processing
+    {
+        public static void Process(string filePath)
+        {
+            double lowThreshold = 40;
+            // Should be between 2 and 3 times larger
+            double highThreshold = 3 * lowThreshold;
+
+            var img = CvInvoke.Imread(filePath, ImreadModes.Grayscale);
+            var imgAfterEdgeDetection = new Mat(img.Rows, img.Cols, DepthType.Cv8U, 1);
+            CvInvoke.Canny(img, imgAfterEdgeDetection, lowThreshold, highThreshold);
+            var imgAfterInvert = new Mat(img.Rows, img.Cols, DepthType.Cv8U, 1);
+            CvInvoke.BitwiseNot(imgAfterEdgeDetection, imgAfterInvert);
+            CvInvoke.Imwrite("output.png", imgAfterInvert);
+        }
+    }
+}
---
+++ Pic2Vector/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Pic2Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numArgs = args.Length;
+            if (numArgs != 1)
+            {
+                Console.WriteLine("Invalid arguments");
+                return;
+            }
+            var fileName = args[0];
+            Processing.Process(fileName);
+
+        }
+    }
+}
